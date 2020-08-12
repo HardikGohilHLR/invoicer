522e7a228ba359849b9d3b8c3f208e55ecd73f17
Hardik Gohil <hardik.gohil@artoon.in>
2020-08-12 19:03:10 +0530

    styles and font added

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router';
 
 Vue.config.productionTip = false;
 
+// SCSS
+import './assets/styles/base.scss';
+import 'currency-flags/dist/currency-flags.css';
+
 new Vue({
   router,
   render: h => h(App)
