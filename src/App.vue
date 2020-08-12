bbe31359564596ccad6e22502c5900b1de50673d
Hardik Gohil <hardik.gohil@artoon.in>
2020-08-12 20:45:28 +0530

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
 	<div id="app">
 		<router-view />
+
+		<!-- Footer -->
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Footer from "@/components/Footer.vue";
 export default {
 	name: 'App',
+	components: {
+		Footer
+	},
 	watch: {
 		// Update Title
 		$route: {
---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+        <div class="container">
+            <div>
+                <p>
+                    &copy; copyright {{ new Date().getFullYear() }}                
+                    <a href="https://www.instagram.com/hardikgohilhlr/" target="_blank">Hardik Gohil</a>    
+                </p>  
+            </div>
+
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'footer'
+}
+</script>
+
+
+
+<style lang="scss">
+footer{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    left: 0;
+    background: #fff;
+    padding: 15px;
+}
+</style>
